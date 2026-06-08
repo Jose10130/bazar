656b0ddc21fd099704c98e7daa686e52361b0ff8
Jose10130 <jose10130.jcd@mail.com>
2026-06-08 04:43:48 -0300

    Fix PostgreSQL config

--- site/src/database/config/config.js
+++ site/src/database/config/config.js
@@ -1,33 +1,46 @@
 module.exports = {
-  "development": {
-    "use_env_variable": "DATABASE_URL",
-    "username": "bazar_db_s472_user",
-    "password": "zK1Y6SQY9pLd0br3LzhuKisHnPQOqTho",
-    "database": "bazar_db_s472",
-    "host": "dpg-d8j65448aovs738vo150-a.oregon-postgres.render.com",
-    "port": 5432,
-    "dialect": "postgres",
-    "dialectOptions": {
-      "ssl": {
-        "require": true,
-        "rejectUnauthorized": false
+  development: {
+    username: "bazar_db_s472_user",
+    password: "zK1Y6SQY9pLd0br3LzhuKisHnPQOqTho",
+    database: "bazar_db_s472",
+    host: "dpg-d8j65448aovs738vo150-a.oregon-postgres.render.com",
+    port: 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
       }
     }
   },
-  "test": {
-    "username": "root",
-    "password": "",
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+
+  test: {
+    username: "bazar_db_s472_user",
+    password: "zK1Y6SQY9pLd0br3LzhuKisHnPQOqTho",
+    database: "bazar_db_s472",
+    host: "dpg-d8j65448aovs738vo150-a.oregon-postgres.render.com",
+    port: 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
-"production": {
-  "use_env_variable": "DATABASE_URL",
-  "dialect": "postgres",
-  "dialectOptions": {
-    "ssl": {
-      "require": true,
-      "rejectUnauthorized": false
+
+  production: {
+    username: "bazar_db_s472_user",
+    password: "zK1Y6SQY9pLd0br3LzhuKisHnPQOqTho",
+    database: "bazar_db_s472",
+    host: "dpg-d8j65448aovs738vo150-a.oregon-postgres.render.com",
+    port: 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
   }
-}}
+};
