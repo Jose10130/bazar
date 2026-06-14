18192c2b344bb158be529e54b4762eadc5bde5e8
Jose C Diaz <150755018+Jose10130@users.noreply.github.com>
2026-06-14 06:34:42 -0300

    Actualizar config.js

--- site/src/db/config/config.js
+++ site/src/db/config/config.js
@@ -28,16 +28,16 @@ development: {
   }
   },
   production: {
-  username: "bazar_db_s472_user",
-  password: "zK1Y6SQY9pLd0br3LzhuKisHnPQ0qTho",
-  database: "bazar_db_s472",
-  host: "dpg-d8j65448aovs738vo150-a.oregon-postgres.render.com",
-  port: 5432,
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+    username: "bazar_db_s472_user",
+    password: "zK1Y6SQY9pLd0br3LzhuKisHnPQ0qTho",
+    database: "bazar_db_s472",
+    host: "dpg-d8j65448aovs738vo150-a.oregon-postgres.render.com",
+    port: 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
   }
-};
