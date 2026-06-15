c877af1f2d92d5a3e1b0197c176190ef8cfc916e
jose10130 <jose10130.jcd@gmail.com>
2026-06-14 21:39:52 -0300

    10

--- site/src/app.js
+++ site/src/app.js
@@ -49,7 +49,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.resolve(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(partials());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
